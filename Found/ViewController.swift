5da2f42d6b925629e3f24d84d220c059ddae804f
Yong Bakos <ybakos@humanoriented.com>
2015-05-26 15:06:19 +0200

    Lesson 4. Zoom in on the current location.

--- Found/ViewController.swift
+++ Found/ViewController.swift
@@ -6,17 +6,29 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 */
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.setUserTrackingMode(.Follow, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,
+            longitude: userLocation.coordinate.longitude)
+        let width = 10000.0 // meters
+        let height = 10000.0
+        let region = MKCoordinateRegionMakeWithDistance(center, width, height)
+        mapView.setRegion(region, animated: true)
     }
     
 }
